d0ef4e7f4c1b34925211dde15a05fd67a5531cd8
imMatheus <matheusmendesbarata03@gmail.com>
2024-03-03 14:38:25 +0100

    started server

--- server/main.go
+++ server/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+
+	"github.com/imMatheus/syntax-city/server/internal/game"
 )
 
+const PORT = ":8080"
+
 func main() {
-	fmt.Printf("Init")
+	lis, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatalf("cannot create listener: %s", err)
+	}
+
+	log.Default().Printf("server is listening on port %s", PORT)
+
+	err = game.StartGameServer(lis)
+
+	if err != nil {
+		log.Fatalf("cannot start game server: %s", err)
+	}
 }
